84de9908ecc0b09267f6a24cca3bb2e8c1a19676
syrianspock <missrisalaheddine@gmail.com>
2014-11-28 20:38:23 +0100

    sorting by error & human readable resistor values

--- vdiv.py
+++ vdiv.py
@@ -1,7 +1,19 @@
 import numpy as np
 
+def format_r (r):
+    """Resistor values in human readable format"""
+    if r >= 1000000:
+        r = "{:.1f}M".format(r / 1000000.0)
+        return r
+    elif r >= 1000:
+        r = "{:.1f}K".format(r / 1000.0)
+        return r
+    else:
+        r = "{:.1f}".format(r)
+        return r
+
 # standard resistor values
-r_values = np.array([1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7,
+r_series = np.array([1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7,
                      3.0,3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5,
                      8.2, 9.1])
 # resistor orders of magnitude
@@ -13,26 +25,30 @@ vout_ideal = float(raw_input('Output voltage (V): '))
 err_tolerance = float(raw_input('Tolerated error (V): '))
 max_current = float(raw_input('Maximum current (mA): '))
 
-# compute all possible combinations
-r_comb_list = []
-for i in r_values:
+# compute resistor values
+r_values = []
+for i in r_series:
     for j in r_factors:
-        for k in r_values:
-            for l in r_factors:
-                r1 = i * 10**j
-                r2 = k * 10**l
-                vout_est = vin_ideal * r1 / (r1 + r2)
-                error = np.linalg.norm(vout_est - vout_ideal)
-                current = vin_ideal / (r1 + r2) * 1000
-                if error <= err_tolerance and current <= max_current:
-                    r1 = float("{0:.2f}".format(r1))
-                    r2 = float("{0:.2f}".format(r2))
-                    error = float("{0:.4f}".format(error))
-                    error_pct = "{0:.4f}".format(error * 100 / vout_ideal) + '%'
-                    r_comb_list.append((r1, r2, error, error_pct))
+        r = i * 10**j
+        r_values.append(r)
+
+# compute combinations
+r_comb_list = []
+for r1 in r_values:
+    for r2 in r_values:
+        vout = vin_ideal * r1 / (r1 + r2)
+        error = np.linalg.norm(vout - vout_ideal)
+        current = 1000 * vin_ideal / (r1 + r2)
+        if error <= err_tolerance and current <= max_current:
+            r_comb_list.append((format_r(r1),
+                                format_r(r2),
+                                "{:.1e}".format(error),
+                                "{:.2%}".format(error / vout_ideal),
+                                "{:.1f}".format(current)))
 
 # sort and print possible combinations
-r_comb_list.sort()
-
+print "All possible combinations"
+print "(R1, R2, Error (V), Error (%), Current (mA)"
+r_comb_list = sorted(r_comb_list, key=lambda x: x[2]) # sort by error
 for i in r_comb_list:
     print i
