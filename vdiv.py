140c19406b1ec19616de8342936402bbb53a7570
Florian Reinhard <florian.reinhard@epfl.ch>
2014-11-28 22:33:29 +0100

    Merge and resistance tolerance control

--- vdiv.py
+++ vdiv.py
@@ -1,7 +1,7 @@
 import numpy as np
 import texttable
 
-def format_r (r):
+def format_r(r):
     """Resistor values in human readable format"""
     if r >= 1000000:
         r = "{:.1f}M".format(r / 1000000.0)
@@ -12,6 +12,13 @@ def format_r (r):
     else:
         r = "{:.1f} ".format(r)
         return r
+
+def error_max(vin_ideal, vout_ideal, r1, r2, tol):
+    err1 = np.absolute(vout_ideal - vin_ideal * (1+tol)*r1 / ((1+tol) * r1 + (1-tol) * r2))
+    err2 = np.absolute(vout_ideal - vin_ideal * (1-tol)*r1 / ((1-tol) * r1 + (1+tol) * r2))
+
+    return max((err1, err2))
+
 
 # standard resistor values
 r_series = np.array([1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7,
@@ -24,6 +31,7 @@ r_factors = np.array([0, 1, 2, 3, 4, 5, 6])
 vin_ideal = float(raw_input('Input voltage (V): '))
 vout_ideal = float(raw_input('Output voltage (V): '))
 err_tolerance = float(raw_input('Tolerated error (V): '))
+r_tol = float(raw_input('Resistance tolerance (%): '))/100.0
 max_current = float(raw_input('Maximum current (mA): '))
 
 # compute resistor values
@@ -40,7 +48,8 @@ for r1 in r_values:
         vout = vin_ideal * r1 / (r1 + r2)
         error = np.linalg.norm(vout - vout_ideal)
         current = 1000 * vin_ideal / (r1 + r2)
-        if error <= err_tolerance and current <= max_current:
+        if error <= err_tolerance and current <= max_current \
+                and error_max(vin_ideal, vout_ideal, r1, r2, r_tol) <= err_tolerance:
             r_comb_list.append((format_r(r1),
                                 format_r(r2),
                                 "{0:.2e}".format(error),
